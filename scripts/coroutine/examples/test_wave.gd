5cd23443279d7ea16cfb192fad327f7a7abdae29
cirno <2992773838@qq.com>
2026-05-28 14:15:49 +0800

    test: add laser enemy to test_wave (phase 3), fix match syntax

--- scripts/coroutine/examples/test_wave.gd
+++ scripts/coroutine/examples/test_wave.gd
@@ -2,6 +2,7 @@ extends StageScript
 class_name TestLevel
 
 const ENEMY_DATA = preload("res://data/enemy_data/test_enemy_data.tres")
+const LASER_ENEMY_DATA = preload("res://data/enemy_data/test_enemy_laser.tres")
 
 var _phase: int = 0
 
@@ -19,5 +20,11 @@ func _on_step(api: StageAPI) -> Variant:
 			api.spawn_enemy(ENEMY_DATA, Vector2(500, -50))
 			_phase = 3
 			return api.seconds(3.0)
+		3:
+			# 激光敌人
+			api.spawn_enemy(LASER_ENEMY_DATA, Vector2(400, -50))
+			_phase = 4
+			return api.seconds(10.0)
 		_:
+			api.clear_all_lasers()
 			return false
