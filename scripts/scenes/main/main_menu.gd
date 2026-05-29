29f0b71d12f5caafe9ec562badedb7e7db1156c1
cirno <2992773838@qq.com>
2026-05-29 23:45:05 +0800

    tweak: larger initial scatter for logo entrance particles

--- scripts/scenes/main/main_menu.gd
+++ scripts/scenes/main/main_menu.gd
@@ -40,7 +40,7 @@ func _deactivate_title() -> void:
 	var tw := create_tween().set_parallel(true)
 	tw.tween_property(_title_container, "modulate:a", 0.0, 0.25)
 	tw.tween_property(_logo.material, "shader_parameter/alpha_mult", 0.0, 0.25)
-	tw.tween_property($GPUParticles2D, "modulate:a", 1.0, 0.25)
+	tw.tween_property($GPUParticles2D, "modulate:a", 0.5, 0.25)
 	tw.tween_callback(_title_container.hide).set_delay(0.25)
 	tw.tween_callback(_logo.hide).set_delay(0.25)
 
@@ -50,7 +50,6 @@ func _activate_title() -> void:
 	_logo.show()
 	_title_container.modulate.a = 0.0
 	_logo.material.set_shader_parameter("alpha_mult", 0.0)
-	$GPUParticles2D.modulate.a = 1.0
 	var tw := create_tween().set_parallel(true)
 	tw.tween_property(_title_container, "modulate:a", 1.0, 0.25)
 	tw.tween_property(_logo.material, "shader_parameter/alpha_mult", 1.0, 0.25)
