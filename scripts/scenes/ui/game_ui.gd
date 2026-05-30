217fc840467a65351ad8687c198d4c24f4e80b0f
cirno <2992773838@qq.com>
2026-05-30 20:34:08 +0800

    fix: set dot_index, slash_index, char_count in GameUI

--- scripts/scenes/ui/game_ui.gd
+++ scripts/scenes/ui/game_ui.gd
@@ -25,6 +25,9 @@ func _make_number_sprite(node_name: String, pos: Vector2, tex: Texture2D = null,
 	ns.z_index = 128
 	ns.digit_count = dcount
 	ns.digit_texture = tex
+	ns.dot_index = 10
+	ns.slash_index = 11
+	ns.char_count = 12  # 0-9 . /
 	add_child(ns)
 	return ns
 
