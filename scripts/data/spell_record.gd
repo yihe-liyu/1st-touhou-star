00c50bd90595b1caa2c0192c5c90f2263be91a12
cirno <2992773838@qq.com>
2026-06-12 22:48:52 +0800

    refactor: SpellRecord 去 phase_data/boss_data, 纯统计

--- scripts/data/spell_record.gd
+++ scripts/data/spell_record.gd
@@ -3,8 +3,6 @@ extends Resource
 class_name SpellRecord
 
 @export var spell_id: String = ""
-@export var phase_data: PhaseData  # 符卡数据引用，练习模式用
-@export var boss_data: BossData    # Boss 数据引用（视觉等）
 @export var attempts: int = 0
 @export var captures: int = 0
 @export var best_score: int = 0
