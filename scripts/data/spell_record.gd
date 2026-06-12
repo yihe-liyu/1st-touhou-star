8bee7d60e26f2c167cd08bf0157080713d721062
cirno <2992773838@qq.com>
2026-06-12 22:33:33 +0800

    feat: SpellRecord +phase_data 引用, 练习模式用

--- scripts/data/spell_record.gd
+++ scripts/data/spell_record.gd
@@ -3,6 +3,7 @@ extends Resource
 class_name SpellRecord
 
 @export var spell_id: String = ""
+@export var phase_data: PhaseData  # 符卡数据引用，练习模式用
 @export var attempts: int = 0
 @export var captures: int = 0
 @export var best_score: int = 0
