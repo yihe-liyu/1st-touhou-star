6f6d7598053d37349c3cb357ad5b7796947f07bb
cirno <2992773838@qq.com>
2026-06-12 20:03:49 +0800

    clean: GameEvents 删 4 个未用信号

--- scripts/autoload/game_events.gd
+++ scripts/autoload/game_events.gd
@@ -1,11 +1,4 @@
-# autoload/game_events.gd
 extends Node
 
-# 信号命名规则：[主体]_[事件] 或 [事件]_[附加信息]
-
 signal enemy_killed(score: int, position: Vector2)
-signal player_damaged(current_hp: int)
 signal player_death()
-signal graze()           # 擦弹
-signal bomb_used()       # Bomb 释放
-signal boss_intro()
