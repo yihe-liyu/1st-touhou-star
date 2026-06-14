54e553ed42b3e959132eb4b5e0a7cf7f10f6c07f
cirno <2992773838@qq.com>
2026-06-14 22:40:58 +0800

    feat: Bubble.side 枚举 LEFT/RIGHT (Inspector 下拉)

--- scripts/data/dialogue_bubble.gd
+++ scripts/data/dialogue_bubble.gd
@@ -2,10 +2,12 @@
 extends Resource
 class_name DialogueBubble
 
+enum Side { LEFT = 0, RIGHT = 1 }
+
 ## 说话者
 @export var speaker: CharacterProfile
-## 站位 0=左 1=右
-@export var side: int = 0
+## 站位 LEFT=0 左列, RIGHT=1 右列
+@export var side: int = Side.LEFT
 ## 内容
 @export var text: String = ""
 ## 表情 key
