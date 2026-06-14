2aa7475b64c912565f6453c1f5119cc18ac52264
cirno <2992773838@qq.com>
2026-06-14 22:41:31 +0800

    fix: @export_enum 做下拉

--- scripts/data/dialogue_bubble.gd
+++ scripts/data/dialogue_bubble.gd
@@ -6,8 +6,8 @@ enum Side { LEFT = 0, RIGHT = 1 }
 
 ## 说话者
 @export var speaker: CharacterProfile
-## 站位 LEFT=0 左列, RIGHT=1 右列
-@export var side: int = Side.LEFT
+## 站位
+@export_enum("LEFT:0", "RIGHT:1") var side: int = Side.LEFT
 ## 内容
 @export var text: String = ""
 ## 表情 key
